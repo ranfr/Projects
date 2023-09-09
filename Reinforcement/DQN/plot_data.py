8829ea8ff054da7d98b9754664dbdc0a3a67597f
RanFR <ranfr@outlook.com>
2023-09-09 14:41:56 +0800

    debug plot_data.py

--- Reinforcement/DQN/plot_data.py
+++ Reinforcement/DQN/plot_data.py
@@ -6,22 +6,19 @@ def read_file(filename):
     data = list()
     with open(filename, "r") as file:
         for line in file:
-            data.append(line.strip())
+            data.append(float(line.strip()))
     return data
 
 
 def plot_fig(list_data):
     len_data = len(list_data)
-    plt.plot(range(len_data), list_data)
+
+    plt.plot(list(range(len_data)), list_data)
     plt.xlabel("Episodes")
     plt.ylabel("Rewards")
     plt.title("DQN on CartPole-v1")
     plt.show()
 
 if __name__ == "__main__":
-    data1 = read_file("./DQN/Data/data1.txt")
+    data1 = read_file("/home/ranfr/Projects/Reinforcement/DQN/Data/data1_.txt")
     plot_fig(data1)
-
-    data2 = read_file("./DQN/Data/data2.txt")
-    plot_fig(data2)
-    
